57f1482bbf3f024c2d39680e305a27e3a5779b4a
CYHvA <94470249+CYHvA@users.noreply.github.com>
2021-11-24 21:56:29 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var btnMenu = document.querySelector("nav i");
+var menuForm = document.querySelector("nav ul");
+
+console.log(btnMenu);
+console.log(menuForm);
+
+btnMenu.addEventListener("click", openMenu);
+btnMenu.addEventListener("click", changeBtn);
+
+function openMenu() {
+	menuForm.classList.toggle("toonMenu");
+}
+
+function changeBtn() {
+	btnMenu.classList.toggle("toonMenu");
+}
